c5fd4469577fe710469b22f5e0f6e03044405430
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-11-01 20:09:32 +0300

    Setup &  Fetching Data From Supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
+import SmoothieCard from "../components/smoothies";
+import supabase from "../config/supabaseClinet";
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select("*");
+
+      if (error) {
+        setFetchError(error.message || "Something went wrong");
+        setSmoothies(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <div key={smoothie.id}>
+                <SmoothieCard smoothie={smoothie} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
